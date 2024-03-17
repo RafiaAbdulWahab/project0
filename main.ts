1c118034aaf6a07b6e353f23f7ef7dac09b858ee
RafiaAbdulWahab <163725684+RafiaAbdulWahab@users.noreply.github.com>
2024-03-17 15:24:09 +0500

    this is my first file

---
+++ main.ts
@@ -0,0 +1,15 @@
+console.log("Hello, World!");
+
+let color = "skyblue";
+console.log(color)
+let color1: string = "red"
+console.log(color1)
+let num: number = 20;
+console.log(num)
+let day: boolean = true;
+console.log(day)
+let color2 = 80;
+console.log(color2)
+let userName: string = "Ahad Jameel";
+userName = "555"
+
